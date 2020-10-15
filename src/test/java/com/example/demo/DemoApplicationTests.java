beb54ef98c515bab3e8467abf33e0a3aca842a01
NoraHille <norahille@web.de>
2020-10-15 13:20:06 +0200

    added code example and and basic tests

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,11 +3,28 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+import static com.jayway.restassured.RestAssured.given;
+
+
+
+
+
 @SpringBootTest
 class DemoApplicationTests {
 
+
+
     @Test
-    void contextLoads() {
+    public void makeSureThatGoogleIsUp() {
+        given().when().get("http://www.google.com").then().statusCode(200);
+    }
+    @Test
+    public void internalHelloTest() {
+        given().when().get("http://localhost:9091/internal/hello").then().statusCode(200);
     }
 
 }
